ab495f5dc2f2ca7fb2e66bb88e89940211a6035a
firlygrsld <144504913+firlygrsld@users.noreply.github.com>
2024-01-17 21:30:38 +0700

    Update tugas 3

---
+++ Tugas-3-LoopingFunction/Tugas3A/looping.dart
@@ -0,0 +1,57 @@
+// soal no 1
+
+// void main() {
+//   print("LOOPING PERTAMA");
+
+//   var i = 0;
+//   while (i < 20) {
+//     i += 2;
+//     print(i.toString() + " - I love coding");
+//   }
+
+//   print("LOOPING KEDUA");
+
+//   var j = 22;
+//   while (j > 0) {
+//     j -= 2;
+//     print(j.toString() + " - I will become a mobile developer");
+//   }
+// }
+
+// soal no 2
+
+// void main() {
+//   for (var i = 1; i <= 20; i++) {
+//     if (i % 2 == 0) {
+//       print(i.toString() + " - Berkualitas");
+//     } else if (i % 3 == 0) {
+//       print(i.toString() + " - I Love Coding");
+//     } else {
+//       print(i.toString() + " - Santai");
+//     }
+//   }
+// }
+
+// soal no 3
+
+// void main() {
+//   for (var i = 0; i < 4; i++) {
+//     String line = " ";
+//     for (var j = 0; j < 8; j++) {
+//       line += "#";
+//     }
+//     print(line);
+//   }
+// }
+
+// soal no 4
+
+// void main() {
+//   for (var i = 0; i <= 7; i++) {
+//     String line = " ";
+//     for (var j = 0; j < i; j++) {
+//       line += "#";
+//     }
+//     print(line);
+//   }
+// }
---
+++ Tugas-3-LoopingFunction/Tugas3B/function.dart
@@ -0,0 +1,54 @@
+// soal no 1
+
+// void main() {
+//   teriak();
+// }
+
+// teriak() {
+//   print("Halo sanbers!");
+// }
+
+// soal no 2
+
+// void main() {
+//   print(kalikan(12, 4));
+// }
+
+// kalikan(num1, num2) {
+//   return num1 * num2;
+// }
+
+// soal no 3
+
+// void main() {
+//   final name = "Ahmad";
+//   final int age = 30;
+//   final String address = "Jln. Malioboro, Yogyakarta";
+//   final String hobby = "Gaming";
+
+//   (introduce(name, age, address, hobby));
+// }
+
+// introduce(name, age, address, hobby) {
+//   return print(
+//       "Nama saya $name umur saya $age tahun, alamat saya di $address, dan saya punya hobby yaitu $hobby!");
+// }
+
+// soal no 4
+
+//import 'dart:io';
+
+// void main() {
+//   print("Masukkan angka = ");
+//   int? i = int.parse(stdin.readLineSync()!);
+
+//   print(faktorial(i));
+// }
+
+// faktorial(i) {
+//   if (i > 0) {
+//     return i * faktorial(i - 1);
+//   } else {
+//     return 1;
+//   }
+// }
