f29271f57b63a49c2f7ff9bd0d6e96c1d58e9e54
firlygrsld <144504913+firlygrsld@users.noreply.github.com>
2024-01-31 21:12:59 +0700

    Update tugas 12

--- sanberappflutter/lib/main.dart
+++ sanberappflutter/lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:sanberappflutter/tugas/Tugas11/LoginScreen.dart';
+import 'package:sanberappflutter/tugas/Tugas12/get_data_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: LoginScreen(),
+      home: GetDataScreen(),
     );
   }
 }
