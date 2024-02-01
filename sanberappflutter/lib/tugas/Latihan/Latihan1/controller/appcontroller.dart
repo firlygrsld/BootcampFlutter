97b9755454a5b5a5a03f145525dfd7901d9bbb8b
firlygrsld <144504913+firlygrsld@users.noreply.github.com>
2024-02-01 21:52:29 +0700

    Update Tugas 14

---
+++ sanberappflutter/lib/tugas/Latihan/Latihan1/controller/appcontroller.dart
@@ -0,0 +1,31 @@
+import 'dart:developer';
+
+import 'package:sanberappflutter/tugas/Latihan/Latihan1/model/postmodel.dart';
+import 'package:sanberappflutter/tugas/Latihan/Latihan1/services/services.dart';
+import 'package:get/get.dart';
+
+class AppController extends GetxController {
+  var getposts = <Postmodel>[].obs;
+  Services services = Services();
+  var postloading = true.obs;
+  @override
+  void onInit() {
+    callpostmethod();
+    super.onInit();
+  }
+
+  callpostmethod() async {
+    try {
+      postloading.value = true;
+      var result = await services.getallposts();
+      if (result != null) {
+        getposts.assignAll(result);
+      } else {
+        print("null");
+      }
+    } finally {
+      postloading.value = false;
+    }
+    update();
+  }
+}
